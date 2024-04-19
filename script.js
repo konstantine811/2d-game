b4703c2f35d6d9dc93f91af5f7aa084b8e9c87ee
k.abramkin <k.abramkin@kernel.ua>
2024-04-19 23:45:14 +0300

    add moving rect player

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 const gravity = 0.5;
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+};
 class Player {
   constructor(position) {
     this.position = position;
@@ -22,6 +33,7 @@ class Player {
   update() {
     this.draw();
     this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
     if (this.position.y + this.height + this.velocity.y < canvas.height) {
       this.velocity.y += gravity;
     } else {
@@ -32,14 +44,43 @@ class Player {
 const player = new Player({ x: 0, y: 0 });
 const player2 = new Player({ x: 200, y: 0 });
 
-console.log(player);
-
 function animate() {
   window.requestAnimationFrame(animate);
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   player2.update();
+  player.velocity.x = 0;
+  if (keys.d.pressed) {
+    player.velocity.x = 1;
+  } else if (keys.a.pressed) {
+    player.velocity.x = -1;
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "w":
+      player.velocity.y = -10;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+});
